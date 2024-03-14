b3866f2e72904bd4cb6f21c994bda0d56ff39472
AlyanQureshi <144194260+AlyanQureshi@users.noreply.github.com>
2024-03-14 17:05:27 -0600

    Finished annotations for DisasterVictim.java

--- edu/ucalgary/oop/DisasterVictimTest.java
+++ edu/ucalgary/oop/DisasterVictimTest.java
@@ -1,3 +1,10 @@
+/**
+ * @author Alyan Qureshi <a href="mailto:muhammad.qureshi4@ucalgary.ca">
+ * muhammad.qureshi4@ucalgary.ca</a>
+ * @version 1.8
+ * @since 1.0
+*/
+
 package edu.ucalgary.oop;
 
 import org.junit.*;
@@ -24,6 +31,7 @@ public class DisasterVictimTest {
         AVML, DBML, GFML, LSML, MOML, PFML, VGML, VJML // These are all possible dietary restrictions
     }
 
+    /** Creates a setup method that will run before every test to ensure consistency */
     @Before
     public void setUp() {
         victim1 = new DisasterVictim(firstName, ENTRY_DATE);
@@ -35,18 +43,21 @@ public class DisasterVictimTest {
         age = 12;
     }
 
+    /** Testing constructor with a valid date to see if it correctly makes an object */
     @Test
     public void testDisasterVictimConstructorWithValidDate() {
         DisasterVictim newVictim = new DisasterVictim(firstName, validDate);
         assertEquals("The constructor was not able to set a valid date for Entry_Date.", validDate, newVictim.getEntryDate());
     }
 
+    /** Testing constructor with an invalid date to see if it correctly throws an Illegal Argument Exception  */
     @Test (expected = IllegalArgumentException.class)
     public void testDisasterVictimConstructorWithInvalidDate() {
         DisasterVictim newVictim = new DisasterVictim(firstName, invalidDate);
         // Expecting to throw Illegal Argument Exception when running this test.
     }
 
+    /** Testing get and set first name to ensure both methods work correctly */
     @Test
     public void testGetAndSetFirstName() {
         String newFirstName = "Micheal";
@@ -54,6 +65,7 @@ public class DisasterVictimTest {
         assertEquals("The method setFirstName did not update and getFirstName did not return the new first name", newFirstName, victim1.getFirstName());
     }
 
+    /** Testing get and set lirst name to ensure both methods work correctly */
     @Test
     public void testGetAndSetLastName() {
         String newLastName = "Egypt";
@@ -61,52 +73,61 @@ public class DisasterVictimTest {
         assertEquals("The method setLastName did not update and getLastName did not return the new last name", newLastName, victim1.getLastName());
     }
 
+    /** Testing whether the method getDateOfBirth accurately return the date of birth  */
     @Test
     public void testGetDateOfBirth() {
         assertEquals("The method getDateOfBirth did not return the accurate date of birth.", victim1.getDateOfBirth(), dateOfBirth);
     }
 
+    /** Testing whether setDateOfBirth updates the date with a valid date  */
     @Test 
     public void testSetDateOfBirthWithValidDate() {
         victim1.setDateOfBirth(validDate);
         assertEquals("The victim's date of birth was not able to be updated.", dateOfBirth, victim1.getDateOfBirth());
     }
 
+    /** Testing whether setDateOfBirth accurately throws an Illegal Argument Exception */
     @Test (expected = IllegalArgumentException.class)
     public void testSetDateOfBirthWithInvalidDate() {
         victim1.setDateOfBirth(invalidDate);
         // Expecting to throw Illegal Argument Exception when running this test.
     }
 
+    /** Testing whether the method getAge returns the correct age */
     @Test
     public void testGetAge() {
         assertEquals("The method getAge did not return the right age.", age, victim1.getAge());
     }
 
+    /** Testing whether the methods setAge accurately updates the age of the victim */
     @Test
     public void testSetValidAge() {
         victim1.setAge(20);
         assertEquals("The method setAge did not set the new age.", 20, victim1.getAge());
     }
 
+    /** Testing whether the method setAge accurately throws an Illegal Argument Exception when the age is not a correct value. */
     @Test (expected = IllegalArgumentException.class)
     public void testSetInvalidAge() {
         victim1.setAge(-20);
         // Expecting to throw Illegal Argument Exception when running this test.
     }
 
+    /** Testing whether setAge accuately calculates age with a date of birth and EntryDate */
     @Test
     public void testSetAgeWithValidDateOfBirthAndEntryDate() {
         victim1.setAge(dateOfBirth, ENTRY_DATE);
         assertEquals("The method setAge did not correctly set the new age with dateOfBirth and EntryDate.", age, victim1.getAge());
     }
 
+    /** Testing whether setAge will accurately throw an Illegal Argument Exception when date of birth and entry date have an incorrect date format */
     @Test (expected = IllegalArgumentException.class)
     public void testSetAgeWithInvalidDateOfBirthAndEntryDate() {
         victim1.setAge("2030-01-01", "2028-01-01");
         // Expecting to throw Illegal Argument Exception when running this test.
     }
 
+    /** Testing whether both functions will run when they are not supposed to (Only one is supposed to run) */
     @Test
     public void testSettingBothAgeAndBirthDate() {
         boolean check = false;
@@ -117,6 +138,7 @@ public class DisasterVictimTest {
         assertFalse("Both methods setAge and setDateOfBirth should not be able to run.", check)
     }
 
+    /** Testing whether the method getAssignedSocialID accurately return the right SocialID */
     @Test
     public void testGetAssignedSocialID() {
         DisasterVictim newVictim = new DisasterVictim("James", "2022-10-25");
@@ -126,21 +148,25 @@ public class DisasterVictimTest {
         assertEquals("The method getAssignedSocialID did not return the expected social ID", socialId, newerVictim.getAssignedSocialID());
     }
 
+    /** Testing whether the method getFamilyConnections returns the right family connections */
     @Test
     public void testGetFamilyConnections() {
         assertEquals("The method getFamilyConnections did not return the right Family Connections.", familyConnections, victim1.getFamilyConnections());
     }
 
+    /** Testing whether the method getMedicalRecords returns the right medical records */
     @Test
     public void testGetMedicalRecords() {
         assertEquals("The method getMedicalRecords did not return the right medical records.", medicalRecords, victim1.getMedicalRecords());
     }
 
+    /** Testing whether the method getPersonalBelongings returns the right personal belongings */
     @Test
     public void testGetPersonalBelongings() {
         assertEquals("The method getPersonalBelongings did not return the right personal belongings.", personalBelongings, victim1.getPersonalBelongings());
     }
 
+    /** Testing whether the method setFamilyConnections accurately updates familyConnections */
     @Test
     public void testSetFamilyConnections() {
         DisasterVictim newVictim1 = new DisasterVictim("Bob", "2020-01-01");
@@ -155,6 +181,7 @@ public class DisasterVictimTest {
         assertEquals("The method setFamilyConnections did not update the family connections.", newFamilyConnections, victim1.getFamilyConnections());
     }
 
+    /** Testing whether the setMedicalRecord accurately sets the medical records to the new medical record  */
     @Test
     public void testSetMedicalRecords() {
         Location newLocation = new Location("Shelter F", "9999 Shelter Ave");
@@ -167,6 +194,7 @@ public class DisasterVictimTest {
         assertEquals("The method setMedicalRecords did not update the medical records.", newRecords, victim1.getMedicalRecords());
     }
 
+    /** Testing whether setPersonalBelongings accurately updates personalBelongings */
     @Test
     public void testSetPersonalBelongings() {
         Supply item1 = new Supply("Granola Bar", 5);
@@ -180,6 +208,7 @@ public class DisasterVictimTest {
         assertEquals("The method setPersonalBelongings did correctly update the personal belongings.", newSupplies, victim1.getPersonalBelongings());
     }
 
+    /** Testing whether the method addPersonalBelonging correctly adds a new Supply object to personalBelongings */
     @Test
     public void testAddPersonalBelonging() {
         Supply newItem = new Supply("Milk", 2);
@@ -187,6 +216,7 @@ public class DisasterVictimTest {
         assertTrue("The method addPersonalBelonging did not add the new personal belonging.", newItem, personalBelongings.contains(newItem));
     }
 
+    /** Testing whether the method removePersonalBelonging correctly removes the Supply object from personalBelongings */
     @Test
     public void testRemovePersonalBelonging() {
         Supply itemToAdd = new Supply("Milk", 2);
@@ -199,6 +229,7 @@ public class DisasterVictimTest {
         assertFalse("The method removePersonalBelonging did not remove the unwanted supply.", unwantedItem, personalBelongings.contains(unwantedItem));
     }
 
+    /** Testing whether the method removeFamilyConnection correctly removes the unwanted relation from familyConnections */
     @Test
     public void testRemoveFamilyConnection() {
         DisasterVictim newVictim1 = new DisasterVictim("Bob", "2020-01-01");
@@ -211,6 +242,7 @@ public class DisasterVictimTest {
         assertFalse("The method removeFamilyConnection did not remove the unwanted relation.", unwantedRelation, familyConnections.contains(unwantedRelation));
     }
 
+    /** Testing whether the method addFamilyConnection correctly adds a new relation to familyConnections */
     @Test
     public void testAddFamilyConnection() {
         DisasterVictim newVictim1 = new DisasterVictim("Bob", "2020-01-01");
@@ -222,6 +254,7 @@ public class DisasterVictimTest {
         assertTrue("The method addFamilyConnection did not add a new family connection.", newRelation, familyConnections.contains(newRelation));
     }
 
+    /** Testing whether the method addMedicalRecord correctly adds a new medical record */
     @Test
     public void testAddMedicalRecord() {
         Location newLocation = new Location("Shelter B", "8888 Shelter Ave");
@@ -232,16 +265,19 @@ public class DisasterVictimTest {
         assertTrue("The method addMedicalRecord did not add a new medical record.", newRecord, medicalRecords.contains(newRecord));
     }
 
+    /** Testing whether the method getEntryDate return the return the right entry date */
     @Test
     public void testGetEntryDate() {
         assertEquals("The method getEntryDate did not return the right entry date.", ENTRY_DATE, victim1.getEntryDate());
     }
 
+    /** Testing whether the method getComments returns the right comments */
     @Test
     public void testGetComments() {
         assertEquals("The method getComments did not return the comments.", comments, victim1.getComments());
     }
 
+    /** Testing whether the method setComments accurately updates the comments */
     @Test
     public void testSetComments() {
         String newComment = "I am waiting for my brother to pick me up from hospital.";
@@ -249,11 +285,13 @@ public class DisasterVictimTest {
         assertEquals("The method setComments did not set the comment correctly.", newComment, victim1.getComments());
     }
 
+    /** Testing whether the method getGender returns the right gender. */
     @Test
     public void testGetGender() {
         assertEquals("The method getGender did not return the right gender.", gender, victim1.getGender());
     }
 
+    /** Testing whether the method setGender correctly updates the gender */
     @Test
     public void testSetGenderWithValidInput() {
         String validGender = "Female";
@@ -261,6 +299,7 @@ public class DisasterVictimTest {
         assertEquals("The method setGender did not update the gender correctly.", validGender, victim1.getGender());
     }
 
+    /** Testing whether the method setGender correctly throws an Illegal Argument Exception */
     @Test (expected = IllegalArgumentException.class) 
     public void testSetGenderWithInvalidInput() {
         String invalidGender = "Rhinosaurus"; //Should be Invalid
@@ -268,12 +307,14 @@ public class DisasterVictimTest {
         // Expecting to throw Illegal Argument Exception when running this test.
     }
 
+    /** Testing whether the method getDietaryRestrictions correctly returns the right dietary restrictions */
     @Test
     public void testGetDietaryRestrictions() {
         assertEquals("The method getDietaryRestrictions did not return the correct dietary restriction.", 
             victim1.getDietaryRestrictions(), mealRestrictions);
     }
 
+    /** Testing wehter the method setDietaryRestrictions correctly updates the mealRestrictions */
     @Test
     public void testSetDietaryRestrictions() {
         DietRestrictions newRestriction = {DietTypes.VGML};
@@ -282,7 +323,7 @@ public class DisasterVictimTest {
             victim1.getDietaryRestrictions(), newRestriction.toString());
     }
 
-    // Testing validateDateFormat with a correct date format
+    /** Testing validateDateFormat with a correct date format */
     @Test
     public void testValidDateFormatWithCorrectFormatDisasterVictim() {
         boolean expectedCheck = true;
@@ -290,11 +331,10 @@ public class DisasterVictimTest {
         assertEquals("Method validateDateFormat returned false for a date that has a correct format.", expectedCheck, actualCheck);
     }
 
-    // Testing validateDateFormat with an incorrect date format
+    /** Testing validateDateFormat with an incorrect date format */
     @Test(expected = IllegalArgumentException.class) 
     public void testValidDateFormatWithIncorrectFormatDisasterVictim() {
         medicalRecord.validateDateFormat("2090-01-01");
         // Expecting IllegalArgumentException due to an invalid date format
     }
-
 }
