d2cd0966af9519ef76df35e7d13af16ab5afc0e7
AlyanQureshi <144194260+AlyanQureshi@users.noreply.github.com>
2024-03-13 23:40:13 -0600

    added comments to MedicalRecordTest.java

--- edu/ucalgary/oop/MedicalRecordTest.java
+++ edu/ucalgary/oop/MedicalRecordTest.java
@@ -13,17 +13,20 @@ public class MedicalRecordTest {
 
     public MedicalRecordTest() {} 
 
+    // Testing constructor with a valid date format
     @BeforeClass
     public void testMedicalRecordConstructorWithValidDate() {
         assertNotNull("Medical Record Constructor did not create a new object.", medicalRecord);
     }
 
+    // Testing constructor with an invalid date format
     @Test(expected = IllegalArgumentException.class)
     public void testMedicalRecordConstructorWithInvalidDate() {
         MedicalRecord invalidMedicalRecord = new MedicalRecord(location, treatmentDetails, inValidDateOfTreatment);
         // Expecting IllegalArgumentException due to invalid date format
     }
 
+    // Testing set location with a new location
     @Test
     public void testSetLocation() {
         Location expectedLocation = new Location("Walmart", "Centre Street NE, Calgary");
@@ -31,16 +34,19 @@ public class MedicalRecordTest {
         assertEquals("Method setLocation() did not update the location.", medicalRecord.getLocation().getName(), expectedLocation.getName());
     }
 
+    // Testing get location
     @Before
     public void testGetLocation() {
         assertEquals("Method getLocation did not return the right location.", location, medicalRecord.getLocation());
     }
 
+    // Testing get treatment details
     @Before
     public void testGetTreatmentDetails() {
         assertEquals("Method getTreatmentDetails did not return the correct treatment details.", treatmentDetails, medicalRecord.getTreatmentDetails());
     }
 
+    // Testing set Treatment Details
     @Test
     public void testSetTreatmentDetails() {
         String newTreatmentDetails = "Finished surgery of ther orthogonal surgery.";
@@ -48,11 +54,13 @@ public class MedicalRecordTest {
         assertEquals("Method setTreatmentDetails did not set the new treatment details.", newTreatmentDetails, medicalRecord.getTreatmentDetails());
     }    
 
+    // Testing get Date of treatment 
     @Before
     public void testGetDateOfTreatment() {
         assertEquals("Method getDateOfTreatment did not return the right date of treatment.", dateOfTreatment, medicalRecord.getDateOfTreatment());
     }
 
+    // Testing set date of treatment with a valid date format
     @Test
     public void testSetDateOfTreatmentWithValidFormat() {
         String newDate = "2023-05-19";
@@ -60,6 +68,7 @@ public class MedicalRecordTest {
         assertEquals("Method setDateOfTreatment did not return the right date.", newDate, medicalRecord.getDateOfTreatment());
     }
 
+    // Testing set date of treatment with an invalid date format
     @Test(expected = IllegalArgumentException.class)
     public void testSetDateOfTreatmentWithInvalidFormat() {
         medicalRecord.setDateOfTreatment(inValidDateOfTreatment);
