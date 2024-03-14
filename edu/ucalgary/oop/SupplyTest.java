5ab9ec9c9b1e6814b6b2612f5a070ed7516e190e
AlyanQureshi <144194260+AlyanQureshi@users.noreply.github.com>
2024-03-13 21:42:47 -0600

    completed SupplyTest.java and added junit and hamcrest

--- edu/ucalgary/oop/SupplyTest.java
+++ edu/ucalgary/oop/SupplyTest.java
@@ -2,3 +2,49 @@ package edu.ucalgary.oop;
 
 import org.junit.*;
 import static org.junit.Assert.*;
+
+public class SupplyTest  {
+
+    public SupplyTest() {}
+
+    // Testing Constructor for Supply
+    @Test
+    public void testSupplyConstructor () {
+        Supply item =  new Supply("Orange", 4);
+        assertNotNull("Supply Constructor did not create a new object for 4 oranges.", item);
+    }
+
+    @Test
+    public void testSupplySetType() {
+        Supply item =  new Supply("Orange", 4);
+        item.setType("Apple");
+        String actualType = item.getType();
+        String expectedType = "Apple";
+        assertEquals("Method setType did not return the expected type.", expectedType, actualType);
+    }
+
+    @Test
+    public void testSupplyGetType() {
+        Supply item = new Supply("Pear", 5);
+        String actualType = item.getType()
+        String expectedType = "Pear";
+        assertEquals("Method getType did not return the expected type.", expectedType, actualType);
+    }
+
+    @Test
+    public void testSupplySetQuantity() {
+        Supply item =  new Supply("Orange", 4);
+        item.setQuantity(2);
+        String actualQuantity = item.getQuantity();
+        String expectedQuantity = 2;
+        assertEquals("Method setQuantity did not return the expected quantity.", expectedQuantity, actualQuantity);
+    }
+
+    @Test
+    public void testSupplyGetQuantity() {
+        Supply item =  new Supply("Orange", 4);
+        String actualQuantity = item.getQuantity();
+        String expectedQuantity = 4;
+        assertEquals("Method getQuantity did not return the expected quantity.", expectedQuantity, actualQuantity);
+    }
+}
